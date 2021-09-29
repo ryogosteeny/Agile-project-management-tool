fbaba2d23f4d3048d732806dc62c9986ada2a782
Ryogo Ito <ryo_go.0907@icloud.com>
2021-09-29 21:27:48 +0900

    eslint,prettier,storybook,chromatic追加及び設定実施

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:react/recommended", "standard", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "no-use-before-define": "off",
+    "react/prop-types": "off",
+  },
+};
