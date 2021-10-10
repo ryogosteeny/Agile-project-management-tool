bcbe5419d118db5edf795d4ebb13c4eda1132f4b
Ryogo Ito <ryo_go.0907@icloud.com>
2021-10-11 00:45:59 +0900

    TextField.tsxを作成

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,12 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+    '../src/components/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
-}
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  babel: async (options) => {
+    options.presets.push('@emotion/babel-preset-css-prop');
+    return options;
+  },
+};
---
+++ src/components/TextField.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const TextProject = () => {
+    return <input type="text" name="projectName" />;
+};
