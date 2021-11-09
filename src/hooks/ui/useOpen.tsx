7798bd45b49ef5fdbc9274297020aec1a00fcd12
Ryogo Ito <ryo_go.0907@icloud.com>
2021-11-09 16:53:21 +0900

    onClickOpenHandler関数名の修正

---
+++ src/hooks/ui/useOpen.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useRef, useState } from 'react';
+
+export const useOpen = () => {
+  const openTargetRef = useRef<HTMLDivElement>(null);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const openEventHandler = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleOutsideClick = (e: Event) => {
+    if (openTargetRef.current && !openTargetRef.current.contains(e.target as Node)) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleOutsideClick);
+    return () => document.removeEventListener('mousedown', handleOutsideClick);
+  }, []);
+
+  return { openTargetRef, isOpen, setIsOpen, openEventHandler };
+};
