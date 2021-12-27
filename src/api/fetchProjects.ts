244ff98eaefbb621da115ac36b63a6dae590a6fb
Ryogo Ito <ryo_go.0907@icloud.com>
2021-12-27 14:18:45 +0900

    fetchProjects.test.tsの削除

--- src/api/fetchProjects.ts
+++ src/api/fetchProjects.ts
@@ -1,7 +1,7 @@
 import { axiosInstance } from '../axiosInstance';
 import { Project } from './types';
 
-export const fetchProjects = async (): Promise<Project[]> => {
+export const fetchProjects = async () => {
   const { data } = await axiosInstance.get<Project[]>(`/projects`);
   return data;
 };
