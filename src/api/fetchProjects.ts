39561b4d91d08d8c29484170029d7708203abab0
Ryogo Ito <ryo_go.0907@icloud.com>
2021-12-27 14:59:48 +0900

    fetchProjects.tsの戻り値の型修正

--- src/api/fetchProjects.ts
+++ src/api/fetchProjects.ts
@@ -1,7 +1,7 @@
 import { axiosInstance } from '../axiosInstance';
 import { Project } from './types';
 
-export const fetchProjects = async () => {
+export const fetchProjects = async (): Promise<Project[]> => {
   const { data } = await axiosInstance.get<Project[]>(`/projects`);
   return data;
 };
